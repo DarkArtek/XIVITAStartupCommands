82c886c159c7a505ae1d78f292b6757866a034f6
darka <luca@ahd-creative.com>
2022-08-22 15:56:20 +0200

    Rebranding

--- StartupCommands/obj/Debug/net5.0-windows/StartupCommands.AssemblyInfo.cs
+++ StartupCommands/obj/Debug/net5.0-windows/StartupCommands.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StartupCommands")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("XIVITAStartupCommands")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("StartupCommands")]
-[assembly: System.Reflection.AssemblyTitleAttribute("StartupCommands")]
+[assembly: System.Reflection.AssemblyProductAttribute("XIVITAStartupCommands")]
+[assembly: System.Reflection.AssemblyTitleAttribute("XIVITAStartupCommands")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
